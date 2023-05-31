0a4a2f25445aaf02e7d4dd2426b5593cedf3a625
Navyadeepx <112097544+Navyadeepx@users.noreply.github.com>
2023-05-31 16:01:08 +0530

    .

--- ptsd.py
+++ ptsd.py
@@ -1,26 +1,34 @@
-import pickle 
+import pickle
+import os 
 from tkinter import *
 from ttkbootstrap import *
 
 #window
 win=Window(themename='my')
 win.geometry('600x400')
+win.minsize(600,400)
+win.attributes('-topmost',True)
+win.title('PTSD')
+win.iconbitmap('logo.ico')
+#menu
+menu=Menu(win,fg='#353535')
+#sub_menu_file
+filemenu=Menu(menu,tearoff=False)
+filemenu.add_command(label='New',command=lambda:print('New'))
+filemenu.add_command(label='Open',command=lambda:print('Open'))
+filemenu.add_command(label='Save',command=lambda:print('Save'))
+filemenu.add_command(label='Save as',command=lambda:print('Save as'))
+filemenu.add_separator()
+filemenu.add_command(label='Quit',command=win.quit)
+menu.add_cascade(label='File',menu=filemenu)
+#sub_menus
+menu.add_command(label='Enter',command=lambda:print('Enter'))
+menu.add_command(label='Search',command=lambda:print('Search'))
+menu.add_command(label='Update',command=lambda:print('Update'))
+menu.add_command(label='Update',command=lambda:print('Update'))
+menu.add_command(label='Help',command=lambda:print('Help'))
 
-t=Notebook(win,width=600, height=400)
-t.pack(fill='both',expand=True)
 
-f1=Frame(t)
-f1.pack()
-f2=Frame(t)
-f2.pack()
-f3=Frame(t)
-f3.pack()
-f4=Frame(t)
-f4.pack()
-
-t.add(f1,text='Enter')
-t.add(f2,text='Search')
-t.add(f3,text='Update')
-t.add(f4,text='Delete')
+win.configure(menu=menu)
 
 win.mainloop()
